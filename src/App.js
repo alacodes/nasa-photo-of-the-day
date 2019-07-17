fc6b0b0d99f66eeef538fcdbf88871886da61c8b
Tracie T <traciecodes@gmail.com>
2019-07-17 15:06:54 -0500

    Step 3: Added state for data from NASA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+
+  const [media, setMedia] = useState("");
+
+  useEffect(() => {
+
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .then(res => setMedia(res.data.url))
+      .catch(error => console.log(error))
+
+  }, []);
+
+  axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log('issue', error);
+    })
+
   return (
     <div className="App">
       <p>
