c7b82e3be3222065a2d653ef0c2b586046c33330
Tracie T <traciecodes@gmail.com>
2019-07-17 15:20:51 -0500

    Step 3: Added header and footer components. Moved data to it's own doc.

--- src/App.js
+++ src/App.js
@@ -4,30 +4,11 @@ import axios from "axios";
 
 function App() {
 
-  const [media, setMedia] = useState("");
-
-  useEffect(() => {
-
-    axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-      .then(res => setMedia(res.data.url))
-      .catch(error => console.log(error))
-
-  }, []);
-
-  axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.log('issue', error);
-    })
 
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        
       </p>
     </div>
   );
